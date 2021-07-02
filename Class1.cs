8afa0f2c18ecf8bd0955837416ec02e07928770b
Saha <t343st@gmail.com>
2021-07-02 21:16:59 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Северо_запад
+{
+    class Class1
+    {
+        public  int[,] Severo_Zapad(int [] VectorM, int[] VectorN, int M, int N, int[,] MatrixB)
+        {
+            for (int i = 0; i < M; i++) 
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (VectorM[i] == VectorN[j] && VectorM[i] != 0 && VectorN[j] != 0)
+                    {
+                        MatrixB[i, j] = VectorM[i];
+                        VectorM[i] = 0;
+                        VectorN[j] = 0;
+                    }
+                    else if (VectorM[i] < VectorN[j] && VectorM[i] != 0 && VectorN[j] != 0)
+                    {
+                        MatrixB[i, j] = VectorM[i];
+                        VectorN[j] -= VectorM[i];
+                        VectorM[i] = 0;
+                    }
+                    else if (VectorM[i] > VectorN[j] && VectorM[i] != 0 && VectorN[j] != 0)
+                    {
+                        MatrixB[i, j] = VectorN[j];
+                        VectorM[i] -= VectorN[j];
+                        VectorN[j] = 0;
+                    }
+                }
+            }
+            return MatrixB;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace Северо_запад
+{
+    class Program
+    {
+        static void ZapolnVectora(int [] vector, string s)
+        {
+            string[] Smas = s.Split(' ');
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = Convert.ToInt32(Smas[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string path = "Входные данные.txt";
+            string[] Data = File.ReadAllLines(path);//Передача данных из файла в массив
+            int M, N;
+            M = Data.Length - 2;
+            string[] Nlenght = Data[2].Split(' ');
+            N = Nlenght.Length;
+            int[] VectorN = new int[N];
+            int[] VectorM = new int[M];
+
+            ZapolnVectora(VectorN, Data[0]);//Заполнение векторов
+            ZapolnVectora(VectorM, Data[1]);
+
+            int[,] MatrixA = new int[M, N];
+            int[,] MatrixB = new int[M, N];
+            for (int i = 0; i < M; i++)// Заполнение матрицы
+            {
+                Nlenght = Data[i + 2].Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    MatrixA[i, j] = Convert.ToInt32(Nlenght[j]);
+                }
+            }
+
+            Class1 SZ = new Class1();
+            MatrixB=SZ.Severo_Zapad(VectorM, VectorN,M,N, MatrixB);
+
+            path = "Ответ.csv";
+            int F = 0;
+            for (int i = 0; i < M; i++) // Подсчёт F
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (MatrixB[i,j]!=0)
+                    {
+                        F = F + MatrixB[i, j] * MatrixA[i, j];
+                    }                
+                }
+            }
+            string s = "F = "+F ;
+            File.WriteAllText(path, s);
+          
+            }
+    }
+}
